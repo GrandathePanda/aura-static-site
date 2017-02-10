39e0229b1d136b3a46f143615bc2353973b4d7c4
cd10012 <cory.dickson@Corys-MacBook-Pro.local>
2017-02-10 14:59:50 -0500

    webpack is configured correctly

--- webpack.config.js
+++ webpack.config.js
@@ -6,45 +6,61 @@ var BUILD_DIR = path.resolve(__dirname, 'build');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  entry: APP_DIR + '/index.js',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-	test : /\.jsx?/,
-	exclude: /node_modules/,
-	include : APP_DIR,
-	loader : 'babel',
-	query:
-	{
-	  presets:['react', 'es2015']
-	}
-      },
-      {
-	test: /\.scss$/,
-	loaders: ['style', 'css', 'sass']
-      },
-      {
-	test: /\.scss$/,
-	loader: ExtractTextPlugin.extract("style",'css!sass!sass-resources')
-      },
-      {
-	test: /\.(jpe?g|png|gif|svg)$/i,
-	loaders: [
-	  'file?hash=sha512&digest=hex&name=[hash].[ext]',
-	  'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+	entry: APP_DIR + '/index.jsx',
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js'
+	},
+	resolve : {
+		extensions: ['', '.js', '.jsx']
+	},
+	module : {
+		loaders : [
+			{
+				test : /\.jsx?/,
+				exclude: /node_modules/,
+				include : APP_DIR,
+				loader : 'babel',
+				query:
+				{
+					presets:['react', 'es2015']
+				}
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				include: APP_DIR,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.scss$/,
+				loaders: [ 'style', 'css', 'sass' ]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+					loaders: [
+						'file?hash=sha512&digest=hex&name=[hash].[ext]',
+						'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+					]
+			}
+		],
+		rules: [{
+			test: /\.scss$/,
+			use: [{
+				loader: "style-loader"
+			}, {
+				loader: "css-loader"
+			}, {
+				loader: "sass-loader",
+				options: {
+					includePaths: [path.resolve(__dirname, 'src/app/components/Header.scss')]
+				}
+			}]
+		}]
+	},
+	plugins: [
+		new ExtractTextPlugin('public/style.css')
 	]
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin('public/style.css', {
-      allChunks: true
-    })
-  ]
 };
 
 module.exports = config;
