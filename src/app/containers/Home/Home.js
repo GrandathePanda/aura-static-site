2cb2021d3be0417ee6aac3f6674bc90db0549a78
cd10012 <ced361@nyu.edu>
2017-08-13 20:29:24 -0400

    Revive canvas

--- src/app/containers/Home/Home.js
+++ src/app/containers/Home/Home.js
@@ -28,6 +28,34 @@ class Home extends Component {
   render(props) {
     return (
       <div>
+        <Particles className="particle-canvas" width={'100%'} height={'100vh'} params={{
+        particles: {
+          number: {
+            value: 200,
+            enable: true,
+            value_area:800
+          },
+          color: {
+            value: '#000000'
+          },
+          opacity: {
+              value: 1
+          },
+          shape: {
+            polygon: {
+              nb_sides: 12
+            }
+          },
+          line_linked: {
+            enable: true,
+            color: "#000000",
+          },
+          move: {
+            enable: true,
+            speed: 1.3
+          }
+        }
+      }} />
         <div className='app-container'>
           <SidebarMenu handleLogin={this.handleLogin} active={this.props.sidebar_active} />
           <div className={this.props.sidebar_active ? 'aura-container pushed' : 'aura-container' }>
