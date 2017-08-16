884ebbf29c8df46a3da5fb12c921f0e59a25647c
Watson Markson <watson@pibrain.io>
2017-08-16 17:56:26 -0400

    add separate development configs (separate from local and prod)

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ const configs = {
   local: {
     apiUrl: 'http://localhost:4200',
   },
+  development: {
+    apiUrl: 'a broken url for now',
+  },
   production: {
     apiUrl: 'https://2dusz11q81.execute-api.us-east-1.amazonaws.com/prod',
   },
